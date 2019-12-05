fa7ede8e01868205385bd8033f3113b04467539f
Ricardo Velasco Salazar <ricardo.pro@outlook.com>
2019-12-04 22:34:29 -0600

    adding landing page block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare function init_plugins();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'GlitchSite';
+  ngOnInit() {
+    init_plugins();
+  }
+
 }
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare function init_plugins();
 
 @Component({
   selector: 'app-landing-page',
@@ -10,6 +11,8 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    init_plugins();
+
   }
 
 }
